c862bbc0eaa5b8a53a0427e3fce086148fe3b8b4
Marcin Środa <marcin.sroda@pubnub.com>
2022-01-10 08:52:50 +0100

    UI-672
    Setting up a project, environment, and keys for PubNub for Android

---
+++ getting-started/build.gradle.kts
@@ -0,0 +1,77 @@
+import com.pubnub.components.buildsrc.Libs
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+tasks.register("wrapper", Wrapper::class){
+    gradleVersion = "7.2"
+}
+
+android {
+    compileSdk = 31
+
+    defaultConfig {
+        applicationId = "com.pubnub.components.example.getting_started"
+        minSdk = 21
+        targetSdk = 31
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+
+        buildConfigField("String", "PUBLISH_KEY", project.property("PUBNUB_PUBLISH_KEY") as String)
+        buildConfigField("String", "SUBSCRIBE_KEY", project.property("PUBNUB_SUBSCRIBE_KEY") as String)
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = rootProject.extra["compose_version"] as String
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation("com.pubnub.components:chat-android:0.1.2")
+
+    implementation(Libs.PubNub.pubnub)
+
+    implementation("androidx.core:core-ktx:1.8.0-alpha01")
+    implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
+    implementation("androidx.compose.material:material:${rootProject.extra["compose_version"]}")
+    implementation("androidx.compose.ui:ui-tooling-preview:${rootProject.extra["compose_version"]}")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
+    implementation("androidx.activity:activity-compose:1.4.0")
+    implementation(Libs.Coil.coil)
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:${rootProject.extra["compose_version"]}")
+    debugImplementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_version"]}")
+}
---
+++ getting-started/src/main/java/com/pubnub/components/example/getting_started/ChatActivity.kt
@@ -0,0 +1,98 @@
+package com.pubnub.components.example.getting_started
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import com.pubnub.api.PNConfiguration
+import com.pubnub.api.PubNub
+import com.pubnub.api.enums.PNLogVerbosity
+import com.pubnub.components.chat.provider.LocalMemberRepository
+import com.pubnub.components.chat.provider.LocalMembershipRepository
+import com.pubnub.components.chat.ui.component.provider.LocalChannel
+import com.pubnub.components.chat.viewmodel.message.MessageViewModel
+import com.pubnub.components.data.member.DBMember
+import com.pubnub.components.data.membership.DBMembership
+import com.pubnub.components.repository.member.DefaultMemberRepository
+import com.pubnub.components.repository.membership.DefaultMembershipRepository
+import com.pubnub.components.example.getting_started.ui.theme.AppTheme
+import com.pubnub.components.example.getting_started.ui.view.Chat
+import com.pubnub.framework.data.ChannelId
+import kotlinx.coroutines.launch
+
+class ChatActivity : ComponentActivity() {
+
+    private lateinit var pubNub: PubNub
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initializePubNub()
+        setContent {
+            AppTheme(pubNub = pubNub) {
+                AddDummyData()
+                Box(modifier = Modifier.fillMaxSize()) {
+                    ChannelView(id = "myCurrentChannel")
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        destroyPubNub()
+        super.onDestroy()
+    }
+
+    private fun initializePubNub(){
+      pubNub  = PubNub(
+            PNConfiguration().apply {
+                publishKey = BuildConfig.PUBLISH_KEY
+                subscribeKey = BuildConfig.SUBSCRIBE_KEY
+                uuid = "uuid-of-current-user"
+                logVerbosity = PNLogVerbosity.NONE
+            }
+        )
+    }
+
+    private fun destroyPubNub(){
+        pubNub.destroy()
+    }
+
+    // Channel view
+    @Composable
+    fun ChannelView(id: ChannelId) {
+        // region Content data
+        val messageViewModel: MessageViewModel = MessageViewModel.defaultWithMediator(id)
+        val messages = remember { messageViewModel.getAll() }
+        // endregion
+
+        CompositionLocalProvider(LocalChannel provides id) {
+            Chat.Content(
+                messages = messages,
+            )
+        }
+    }
+
+    @Composable
+    fun AddDummyData(vararg channelId: ChannelId) {
+
+        // Create a user object with UUID
+        val memberRepository: DefaultMemberRepository = LocalMemberRepository.current
+        val member: DBMember = DBMember(id = pubNub.configuration.uuid, name = "You", profileUrl = "https://picsum.photos/seed/${pubNub.configuration.uuid}/200")
+
+        // Create a membership for subscribe purpose
+        val membershipRepository: DefaultMembershipRepository = LocalMembershipRepository.current
+        val memberships: Array<DBMembership> = channelId.map { id -> DBMembership(channelId = id, memberId = member.id) }.toTypedArray()
+
+        // Fill database with member and memberships data
+        val scope = rememberCoroutineScope()
+        LaunchedEffect(null) {
+            scope.launch {
+                memberRepository.add(member)
+                membershipRepository.add(*memberships)
+            }
+        }
+    }
+}
