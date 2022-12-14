c74bca2e750fa9a52cb0e4308c7436f058a78c33
mateusz-cichosz <102806048+mateusz-cichosz@users.noreply.github.com>
2022-12-14 11:32:53 +0100

    update chat components and remove RepositoryProvider from local files (#24)

--- telehealth-example/src/main/java/com/pubnub/components/example/telehealth/LoginActivity.kt
+++ telehealth-example/src/main/java/com/pubnub/components/example/telehealth/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.pubnub.components.asPubNub
+import com.pubnub.components.chat.provider.RepositoryProvider
 import com.pubnub.components.example.telehealth.ui.view.Login
 
 class LoginActivity : ComponentActivity() {
