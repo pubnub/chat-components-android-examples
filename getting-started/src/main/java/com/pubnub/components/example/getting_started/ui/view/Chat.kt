c38ab34f63c049edc3339708a447bf3ed546a742
Marcin Środa <marcin.sroda@pubnub.com>
2022-12-16 07:32:18 +0100

    Revert

--- getting-started/src/main/java/com/pubnub/components/example/getting_started/ui/view/Chat.kt
+++ getting-started/src/main/java/com/pubnub/components/example/getting_started/ui/view/Chat.kt
@@ -1,7 +1,6 @@
 package com.pubnub.components.example.getting_started.ui.view
 
 
-import android.os.Message
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Column
@@ -9,30 +8,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewModelScope
-import androidx.paging.*
-import com.pubnub.components.chat.provider.LocalMessageRepository
+import androidx.paging.PagingData
 import com.pubnub.components.chat.ui.component.input.MessageInput
-import com.pubnub.components.chat.ui.component.member.MemberUi
 import com.pubnub.components.chat.ui.component.message.MessageList
 import com.pubnub.components.chat.ui.component.message.MessageUi
 import com.pubnub.components.chat.ui.component.presence.Presence
 import com.pubnub.components.chat.ui.component.provider.LocalChannel
 import com.pubnub.components.chat.viewmodel.message.MessageViewModel
-import com.pubnub.components.data.message.DBMessage
-import com.pubnub.components.example.getting_started.Settings
-import com.pubnub.components.repository.message.DefaultMessageRepository
-import com.pubnub.components.repository.message.MessageRepository
 import com.pubnub.framework.data.ChannelId
-import com.pubnub.framework.util.toTimetoken
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.Flow
 
 object Chat {
 
@@ -68,7 +57,6 @@ object Chat {
         }
     }
 
-    @OptIn(ExperimentalPagingApi::class)
     @Composable
     fun View(
         channelId: ChannelId,
@@ -85,53 +73,8 @@ object Chat {
                 messages = messages,
             )
         }
-
-        val messageRepository = LocalMessageRepository.current as DefaultMessageRepository
-        LaunchedEffect(channelId){
-            send(messageRepository, fakeData(channelId, 1))
-            send(messageRepository, fakeData(channelId, 2))
-            delay(5_000L)
-
-//            send(messageRepository, fakeData(channelId, 10))
-//            send(messageRepository, fakeData(channelId, 9))
-//            delay(5_000L)
-//
-//            send(messageRepository, fakeData(channelId, 8))
-//            send(messageRepository, fakeData(channelId, 7))
-//            delay(5_000L)
-
-            send(messageRepository, fakeData(channelId, 6))
-            send(messageRepository, fakeData(channelId, 5))
-            delay(5_000L)
-
-            send(messageRepository, fakeData(channelId, 4))
-            send(messageRepository, fakeData(channelId, 3))
-            delay(5_000L)
-        }
     }
 }
-
-private suspend fun send(messageRepository: DefaultMessageRepository, message: MessageUi.Data){
-    val dbMessage = DBMessage(
-        id = message.uuid,
-        text = message.text,
-        contentType = message.contentType,
-        content = message.content,
-        custom = message.custom,
-        publisher = message.publisher.id,
-        published = message.published,
-        channel = message.channel,
-        timetoken = message.published,
-        isSent = false,
-        exception = null,
-    )
-
-    // Add message to repository
-    messageRepository.insertOrUpdate(dbMessage)
-}
-
-private fun fakeData(channelId: ChannelId, i: Int) = MessageUi.Data("$i", MemberUi.Data(Settings.userId, ""), channelId, "$i", "$i", i.toLong(), i.toLong(), false, false, listOf(), "")
-
 
 @Composable
 @Preview
